79ec00c19fb6b85e97dc8776867bbcd72fa84c51
AqingCyan <aqingcyan@cyanthing.com>
2021-08-09 00:39:14 +0800

    feat: like & cancel like

---
+++ src/like/like.router.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import * as likeController from './like.controller';
+import { authGuard } from '../auth/auth.middlerware';
+
+const router = express.Router();
+
+/**
+ * 保存用户点赞内容
+ */
+router.post('/posts/:postId/like', authGuard, likeController.storeUserLikePost);
+
+/**
+ * 取消点赞内容
+ */
+router.delete(
+  '/posts/:postId/like',
+  authGuard,
+  likeController.destroyUserLikePost,
+);
+
+export default router;
