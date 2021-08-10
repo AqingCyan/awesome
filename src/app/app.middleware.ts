a1b6c6f8e8592e928dc2a2795784061f7a7c2774
AqingCyan <aqingcyan@cyanthing.com>
2021-08-10 21:23:26 +0800

    feat: get user info by id

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -81,6 +81,10 @@ export const defaultErrorHandler = (
       statusCode = 404;
       message = '没找到～ 🦖';
       break;
+    case 'USER_NOT_FOUND':
+      statusCode = 404;
+      message = '没找到这个用户～～';
+      break;
     default:
       statusCode = 500;
       message = '服务暂时出了一些问题～～ 🌴';
